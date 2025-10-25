6698b77764154198975c54528ecd17e0d31f898b
Nevxs <lucashnevessousa@gmail.com>
2025-10-24 21:14:34 -0300

    feat: alteração da view home

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.app, name='app'),
+    path('', views.todo_list, name='app'),
     path('todo_list', views.todo_list, name='todo_list'),
     
     # URL da API (JSON)
